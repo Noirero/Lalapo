6553c109edc0b4dcb9c8b467b05a434b1a3faa21
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-25 10:09:46 +0700

    Format Extensions routing change

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/BrowseTab.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/BrowseTab.kt
@@ -16,8 +16,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalContext
@@ -52,7 +52,6 @@ import tachiyomi.i18n.MR
 import tachiyomi.presentation.core.i18n.stringResource
 import tachiyomi.presentation.core.util.collectAsState
 import kotlin.math.abs
-
 
 data object BrowseTab : Tab {
 
