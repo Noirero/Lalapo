550c71f675b4b3137fafee679d2b084ba8d2fe28
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-26 13:42:39 +0700

    fix: restore storage settings effect import

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsDataScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsDataScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
